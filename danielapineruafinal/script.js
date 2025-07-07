dfe4f539a8c079e3350d965b5fa66108bad5308a
danielapinerua <daniela.pinerua@davinci.edu.ar>
2025-07-07 17:30:54 -0300

    s

--- danielapineruafinal/script.js
+++ danielapineruafinal/script.js
@@ -10,3 +10,7 @@ function verActores() {
     boton.classList.add("abierto");
   }
 }
+  function mostrarSpotify() {
+    var caja = document.getElementById("spotify");
+    caja.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/25vto1XDKIDTzqxBUi7U32?utm_source=generator" width="100%" height="80" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
+  }
