bacc6516b68f3883bb46a5aa912972170788fc5b
danielapinerua <daniela.pinerua@davinci.edu.ar>
2025-07-09 14:42:32 -0300

    m

--- danielapineruafinal/script.js
+++ danielapineruafinal/script.js
@@ -14,3 +14,14 @@ function verActores() {
     var caja = document.getElementById("spotify");
     caja.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/25vto1XDKIDTzqxBUi7U32?utm_source=generator" width="100%" height="80" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
   }
+  function cambiarColores() {
+  var cuadro = document.getElementById("cuadro");
+  var mapa = document.getElementById("mapa");
+  var columna1 = document.getElementById("columna1");
+  var columna2 = document.getElementById("columna2");
+
+  cuadro.style.backgroundColor = "#BDD9F2"; 
+  mapa.style.border = "3px solid white";
+  columna1.style.backgroundColor = "white";
+  columna2.style.backgroundColor = "white";
+}
